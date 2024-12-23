21fd268adf5cc4e43bf14df1ce89106f70bb7669
Jon Gewirtzman <jonathan.gewirtzman@yale.edu>
2024-12-23 11:44:38 -0500

    fix

--- methane_cycling/black_oak_methanome_abundance.R
+++ methane_cycling/black_oak_methanome_abundance.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2048256df7b45092cc68fea224c88299364b47174d82fa5bf75235a460c9daa
-size 4608
+oid sha256:3c7d559d93d51393b33c29c1dee0c7c7dd3b14260598d520a2b5f0dd786153c5
+size 3987
