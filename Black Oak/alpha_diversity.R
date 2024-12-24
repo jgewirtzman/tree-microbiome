7468531a3fba2b7d8fab6e719fac2b536b17ebcc
Jon Gewirtzman <jonathan.gewirtzman@yale.edu>
2024-12-24 18:53:23 -0500

    fix plotting

--- Black Oak/alpha_diversity.R
+++ Black Oak/alpha_diversity.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60880998287323011e26b73fbcc8831a131558fea39dc2835e21dfd2921a1afc
-size 5914
+oid sha256:bf7ffef6e794c5cc7fd41f7cc3d153a5e2b3f349ba6a27504dc0fc8223220a73
+size 6272
