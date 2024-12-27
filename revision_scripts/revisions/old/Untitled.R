4897818744358fc3bfb4a76d368a273b169198bb
Jon Gewirtzman <jonathan.gewirtzman@yale.edu>
2024-12-27 18:40:20 -0500

    new scripts for revision

---
+++ revision_scripts/revisions/old/Untitled.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:11023a4c3eca3bb49d159ccdb6733a66d4484f2b35756232ca991a20a1a53ef0
+size 1844
---
+++ revision_scripts/revisions/tissue_gases/additional_figs.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:81af0e3c9dda8aac713a3dce35676e17ac76ccf094a278371ce5331ce0bb231a
+size 5782
