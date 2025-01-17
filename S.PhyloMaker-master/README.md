881d8f00b769ca4ba5349d05ddf8cfc2de0fbd47
Jon Gewirtzman <jonathan.gewirtzman@yale.edu>
2025-01-17 17:42:46 -0500

    add phylomaker. clean up scripts for stats and figures in revision. consolidate folders. clean up old scripts.

---
+++ S.PhyloMaker-master/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:684d87b8f3d8bdc3c53075b158c89493f21a20e7375183bbf67c55363685ed39
+size 4671
---
+++ revision_scripts/tissue_gases/01_tissue_calc_stats.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f99f68c3e8ccd17b06d49211eceea880064abe173c3eef130299b72332cb907b
+size 11360
