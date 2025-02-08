2af98b6a0a0704e60c107133a7185f61a0794b74
Jon Gewirtzman <jonathan.gewirtzman@yale.edu>
2025-02-08 08:22:08 -0500

    fix sidak letters, update gitignore

--- revision_scripts/group_distances/working_16s.R
+++ revision_scripts/group_distances/working_16s.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:221616626864e2fbf8378aee4bbcc60b48f8e4229942d7a4fc78a19006ccfc9b
-size 13825
+oid sha256:6b0dfa4f1209d4c13b2f60f40f1f003f5f89b2a49c641628c808067d4e09491d
+size 13839
